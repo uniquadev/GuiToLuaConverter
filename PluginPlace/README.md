cac6fdf6aed04c85e392637db1878e217461cbd7
uniquadev <uniquadev@gmail.com>
2022-07-04 23:38:40 +0100

    plugin structure with a simple convert btn feature

--- PluginPlace/README.md
+++ PluginPlace/README.md
@@ -1,5 +1,3 @@
 ## GuiToLuaConverter
 
 vscode build with `CTRL+SHIFT+P`
-
-> TODO: make an util script that store the plugin inside the RobloxStudio Plugin folder every build for development purpose
--- PluginPlace/src/plugin/init.server.lua
+++ PluginPlace/src/plugin/init.server.lua
@@ -1,1 +1,19 @@
-print('Hello World')
+-- Modules
+-- Plugin UI
+local Toolbar = plugin:CreateToolbar("GuiToLuaConverter");
+local ConvertBtn = Toolbar:CreateButton("Start Convertion", "Convert the selected ScreenGui", "rbxassetid://3526632592");
+-- Plugin Core
+local function Convert()
+    print('Hello World')
+    -- TODO: check if ScreenGui selected
+    -- TODO: make a GuiToLuaConverter folder in the workspace
+    -- TODO: start convertion
+    -- TODO: check errors
+    -- TODO: instanciate a "ScreenGui.Name .. os.time"  disabled LocalScript in the out folder
+    -- TODO: save script (since roblox doesn't let plugins write how many chars they want in a script
+    -- it will require us to split the out code in more scripts and let the user copy paste them in one..
+    -- ty roblox
+    -- TODO: select the instanciated script
+end;
+-- Connections
+ConvertBtn.Click:Connect(Convert);
