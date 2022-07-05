8e34314fe1f66a121ae489cd9bde609089d2cd1d
iK4oS <github.s41jz@simplelogin.co>
2022-07-05 14:08:57 +0200

    Update SanitizeDump

--- RobloxDumper/dump.py
+++ RobloxDumper/dump.py
@@ -1,23 +1,24 @@
 def SanitizeDump(DumpJSON:dict) -> dict[str, list[str]]:
     """Parse the json and remove all useless data such ReadOnly properties and more.."""
 
-    JSON = {} # ClassName: {Superclass: ClassName1, Members: [Property1, Property2 ...]}
+    Properties : list[str] = []
     
     for DumpObj in DumpJSON['Classes']: # loop trought the dump and save important instances
         # continue cases
-        if 'Tags' in DumpObj and "Deprecated" in DumpObj['Tags']:
-            continue
-        # sanitized class obj
-        Members : list[str] = []
-        ClassObj = {
-            'Superclass': DumpObj['Superclass'],
-            'Members': Members
-        }
-        # load all members
+        
         for Member in DumpObj['Members']:
-            Members.append(Member['Name'])
-        # store sanitized class obj
-        JSON[DumpObj['Name']] = ClassObj
+            
+            if 'Tags' in DumpObj:
+                if "Deprecated" in DumpObj['Tags'] or "ReadOnly" in DumpObj['Tags']:
+                    continue
+                
+            if Member["MemberType"] != "Property":
+                continue
+            
+            # Using .Capitalize() to makes only the first letter of the name capital
+            # 'canvasSize' => 'Canvassize' but we need 'CanvasSize'       
+            CapitalizedName = Member["Name"][0].upper() + Member["Name"][1:]
+            Properties.append(CapitalizedName)
 
-    return JSON
+    return Properties
     
