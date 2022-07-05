19465380f2a87a1105c209842eae2e398d5b6497
uniquadev <uniquadev@gmail.com>
2022-07-05 10:06:57 +0100

    debug workflow

--- PluginPlace/src/main.server.lua
+++ PluginPlace/src/main.server.lua
@@ -1,22 +1,32 @@
--- Modules
+--// REQUIRES \\--
+local Utils = require(script.Parent:WaitForChild("utils"));
+local RbxApi = require(script.Parent:WaitForChild("rbxapi")); -- Debug purpose
+--// GLOBALS \\--
+local DEBUG = true;
 -- Services
 local Selection = game:GetService("Selection");
 
 -- Plugin UI
 local Toolbar = plugin:CreateToolbar("GuiToLuaConverter");
-local ConvertBtn = Toolbar:CreateButton("Start Convertion", "Convert the selected ScreenGui", "rbxassetid://3526632592");
+local ConvertBtn = Toolbar:CreateButton(
+    "Start Convertion", "Convert the selected ScreenGui", "rbxassetid://3526632592"
+);
+local DebugBtn = DEBUG and Toolbar:CreateButton(
+    "Debug", "Start a debug workflow", "rbxassetid://4525004810" -- orbital easteregg <3
+);
 
 -- Plugin Core
 local function Convert()
-    local CurrentlySelectedParts = Selection:Get();
-    if (#CurrentlySelectedParts ~= 1 or CurrentlySelectedParts[1].ClassName ~= "ScreenGui") then
+    local SelectedParts = Selection:Get();
+    local Selected : ScreenGui = SelectedParts[1];
+    -- check if selected instance is a screen gui
+    if (not Selected or Selected.ClassName ~= "ScreenGui") then
         warn("Please select a ScreenGui");
         return;
     end;
-    local ScreenGui = CurrentlySelectedParts[1];
+    -- out folder
+    local OutFolder = Utils.GetOutFolder(Selected.Name);
 
-    local GuiToLuaConverterFolder = Instance.new("Folder", game.Workspace);
-    GuiToLuaConverterFolder.Name = "GuiToLuaConverter";
 
 
     -- TODO: start convertion
@@ -27,5 +37,21 @@ local function Convert()
     -- ty roblox
     -- TODO: select the instanciated script
 end;
+
+--// DEBUG WORKFLOW \\--
+local function PropertiesCheck()
+    local Members = RbxApi.GetProperties('TextLabel');
+    local Dummy = RbxApi.GetDummy('TextLabel');
+    assert(Members, 'Can\'t retrive TextLabel properties.');
+    assert(Dummy, 'Dummy not instanciated.');
+    assert(Members['Text'], 'TextLabel.Text not found.');
+    assert(Members['Text'] == Dummy.Text, ('TextLabel.Text not EQ to %s.'):format(Dummy.Text));
+    print('ROBLOX-API working.')
+end
+
 -- Connections
 ConvertBtn.Click:Connect(Convert);
+
+if DebugBtn then
+    DebugBtn.Click:Connect(PropertiesCheck);
+end
