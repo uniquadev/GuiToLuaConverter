b56e48a1a941d133d19cac628fb95db46e62fb09
uniquadev <uniquadev@gmail.com>
2022-07-05 08:21:42 +0100

    working SanitizeDump

--- RobloxDumper/dump.py
+++ RobloxDumper/dump.py
@@ -1,17 +1,23 @@
-def SanitizeDump(DumpJSON:dict) -> list:
+def SanitizeDump(DumpJSON:dict) -> dict[str, list[str]]:
     """Parse the json and remove all useless data such ReadOnly properties and more.."""
 
-    properties = []
+    JSON = {} # ClassName: {Superclass: ClassName1, Members: [Property1, Property2 ...]}
     
-    for i in range(len(DumpJSON["Classes"])):
-        for j in DumpJSON["Classes"][i]["Members"]:
-            if j["MemberType"] != "Property":
-                continue
-                
-            if "Tags" in j:
-                if "ReadOnly" in j["Tags"] or "Deprecated" in j["Tags"]:
-                    continue    
-            properties.append(j["Name"].capitalize()) 
+    for DumpObj in DumpJSON['Classes']: # loop trought the dump and save important instances
+        # continue cases
+        if 'Tags' in DumpObj and "Deprecated" in DumpObj['Tags']:
+            continue
+        # sanitized class obj
+        Members : list[str] = []
+        ClassObj = {
+            'Superclass': DumpObj['Superclass'],
+            'Members': Members
+        }
+        # load all members
+        for Member in DumpObj['Members']:
+            Members.append(Member['Name'])
+        # store sanitized class obj
+        JSON[DumpObj['Name']] = ClassObj
 
-    return properties
+    return JSON
     
