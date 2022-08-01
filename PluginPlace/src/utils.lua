3681a9a39a8bb6d7800dbe5b7c6f91e90bcc2346
uniquadev <uniquadev@gmail.com>
2022-08-01 14:09:19 +0100

    split support

--- PluginPlace/src/utils.lua
+++ PluginPlace/src/utils.lua
@@ -31,10 +31,30 @@ Utils = {
     -- limit the write to the buffer
     WriteConvertionRes = function(Res:G2L.ConvertionRes) : Folder
         local Out = Utils.GetOutFolder(Res.Gui.Name);
-        local LocalScript = Instance.new('LocalScript', Out);
-        LocalScript.Disabled = true;
-        LocalScript.Source = Res.Source;
-        -- TODO split support
+        -- split support
+        local Parts = {};
+        local Idx = 0;
+        local SplitSize = 100000;
+        while true do
+            local Part = Res.Source:sub((SplitSize * Idx) + 1, SplitSize*(Idx+1));
+            if Part == '' then
+                break;
+            end;
+            table.insert(Parts, Part);
+            Idx += 1;
+        end;
+        local Integrity = pcall(function()
+            for i, Source in next, Parts do
+                local LocalScript = Instance.new('LocalScript', Out);
+                LocalScript.Name = tostring(i);
+                LocalScript.Disabled = true;
+                LocalScript.Source = Source;
+            end
+        end);
+        if not Integrity then
+            warn("Can't write the converted script in the LocalScript.");
+        end
+        -- TODO 
         return Out;
     end;
 }
