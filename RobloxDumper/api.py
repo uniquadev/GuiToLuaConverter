59f66b87b950705085f90c7640f39249bbc1a723
Master Oogway <125769645+ActualMasterOogway@users.noreply.github.com>
2024-08-24 14:14:01 +0200

    updated to version 0.639.0.6390692

--- RobloxDumper/api.py
+++ RobloxDumper/api.py
@@ -1,14 +1,17 @@
-"""Locate roblox studio binary and perform an api execution"""
-import os
+#"""Locate roblox studio binary and perform an api execution"""
+"""perform a http request to max's latest api dump"""
+#import os
+import requests
 import json
 
 class RobloxStudioApi:
-    BinaryPath : None | str = r"C:\Users\%username%\AppData\Local\Roblox\Versions\version-e2bc56a1e4374ca0\RobloxStudioBeta.exe"
+    BinaryPath : None | str = r"C:\Users\%username%\AppData\Local\Roblox\Versions\version-e2bc56a1e4374ca0\RobloxStudioBeta.exe" # new binary path should be r"%localappdata%\Roblox Studio\RobloxStudioBeta.exe"
     def __init__(self) -> None:
         pass
     
     def DumpJSON(self) -> dict:
-        os.system(f"{self.BinaryPath} -API api_dump.json")
-        if not os.path.exists("api_dump.json"):
-            raise FileNotFoundError("api_dump.json not found")
-        return json.load(open("api_dump.json"))
+        #os.system(f"{self.BinaryPath} -API api_dump.json")
+        #if not os.path.exists("api_dump.json"):
+        #    raise FileNotFoundError("api_dump.json not found")
+        #return json.load(open("api_dump.json"))
+        return json.loads(requests.get("https://raw.githubusercontent.com/MaximumADHD/Roblox-Client-Tracker/roblox/API-Dump.json").text)
