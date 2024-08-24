a906e28c7bcc829dcb3a5a55adcd3a6c6d765ac4
Master Oogway <125769645+ActualMasterOogway@users.noreply.github.com>
2024-08-24 18:56:19 +0200

    Made some big changes to the dumper, updated version version-e60bca3482fe488a

--- RobloxDumper/main.py
+++ RobloxDumper/main.py
@@ -1,11 +1,22 @@
-from api import *
+from pick import pick
 from dump import SanitizeDump
 from pathlib import Path
+import json
+
+# Import necessary classes/functions from api_v2 and api
+from api_v2 import RobloxStudioApi as RobloxStudioApiV2
+from api import RobloxStudioApi as RobloxStudioApiLegacy
 
 DUMP_JSON = Path(__file__).parent.parent.joinpath("PluginPlace/src/assets/dump.json").absolute()
 
 def main():
-    Api = RobloxStudioApi()
+    version, index = pick(["V2", "V1"], 'Please choose what API Dumper you want to use:')
+    
+    if index == 0:
+        Api = RobloxStudioApiV2()
+    elif index == 1:
+        Api = RobloxStudioApiLegacy()
+    
     DumpJSON = Api.DumpJSON()
     JSON = SanitizeDump(DumpJSON)
     
@@ -13,6 +24,5 @@ def main():
     with open(DUMP_JSON, "w+") as f:
         f.write(json.dumps(JSON, indent=4))
     
-
 if __name__ == '__main__':
     main()
