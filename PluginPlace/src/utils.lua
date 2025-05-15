36fb81f45f471de270bd6f74534924be0e6c2dc9
Master Oogway <125769645+ActualMasterOogway@users.noreply.github.com>
2025-05-15 16:12:08 +0200

    Using `ScriptEditorService:UpdateSourceAsync´ to bypass the 100k character limit

--- PluginPlace/src/utils.lua
+++ PluginPlace/src/utils.lua
@@ -31,25 +31,12 @@ Utils = {
     -- limit the write to the buffer
     WriteConvertionRes = function(Res:G2L.ConvertionRes) : Folder
         local Out = Utils.GetOutFolder(Res.Gui.Name);
-        -- split support
-        local Parts = {};
-        local Idx = 0;
-        local SplitSize = 100000;
-        while true do
-            local Part = Res.Source:sub((SplitSize * Idx) + 1, SplitSize*(Idx+1));
-            if Part == '' then
-                break;
-            end;
-            table.insert(Parts, Part);
-            Idx += 1;
-        end;
         local Integrity = pcall(function()
-            for i, Source in next, Parts do
-                local LocalScript = Instance.new('LocalScript', Out);
-                LocalScript.Name = tostring(i);
-                LocalScript.Disabled = true;
-                LocalScript.Source = Source;
-            end
+            local LocalScript = Instance.new('LocalScript', Out);
+            LocalScript.Name = "ConvertedScript";
+            game:GetService("ScriptEditorService"):UpdateSourceAsync(LocalScript, function()
+                return Res.Source
+            end);
         end);
         if not Integrity then
             warn("Can't write the converted script in the LocalScript.");
